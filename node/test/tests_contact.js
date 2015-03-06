97a39708ba7b54ae594f987a8b36c47f5c090c8c
Nicolas Pécot <nicolas.pecot@gmail.com>
2015-03-06 20:41:52 +0100

    modifs node /contacts.js modifs tests

--- node/test/tests_contact.js
+++ node/test/tests_contact.js
@@ -1,3 +1,43 @@
 /**
- * Created by Shadow on 06/03/2015.
+ * Created by Shadow on 05/03/2015.
  */
+
+var contact = require('../contact.js'),
+    assert = require('assert');
+
+describe('Tests des fonctionnalités de Contact', function () {
+    'use strict';
+    var contactTest = new contact();
+
+    describe('Test de setIdent', function () {
+        it('should be equal', function () {
+            contactTest.setIdent('1');
+            assert.equal('1', contactTest.ident);
+            assert.notEqual('2', contactTest.ident);
+        });
+        it('should not be equal', function () {
+            contactTest.setIdent('1');
+            assert.notEqual('2', contactTest.ident);
+        });
+    });
+    describe('Test de getIdent', function () {
+        it('should be equal', function () {
+            assert.equal('1', contactTest.getIdent());
+        });
+        it('should not be equal', function () {
+            assert.notEqual('2', contactTest.getIdent());
+        });
+    });
+    describe('Test de setEmail', function () {
+        it('should be equal', function () {
+            contactTest.setEmail('lolcat.truc@kaka.fr');
+            assert.equal('lolcat.truc@kaka.fr', contactTest.email);
+        });
+        it('should not return false', function () {
+            assert.notEqual(false, contactTest.setEmail('lolcat.truc@kaka.fr'));
+        });
+        it('should return false', function () {
+            assert.equal(false, contactTest.setEmail('bonjour'));
+        });
+    });
+});
