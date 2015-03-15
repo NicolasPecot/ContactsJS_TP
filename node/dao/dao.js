4a043bf115eb0a189439456839544ebdca6b8e38
Nicolas Pécot <nicolas.pecot@gmail.com>
2015-03-16 00:36:41 +0100

    Ajout code dao

--- node/dao/dao.js
+++ node/dao/dao.js
@@ -2,9 +2,29 @@
  * Created by Shadow on 07/03/2015.
  */
 (function () {
-    var mongoose = require('mongoose');
+    var mongoose = require('mongoose'),
+        db,
+        schema,
+        dbContact;
+
+    exports.connection = function (connexionString) {
+        mongoose.connect(connexionString); //'mongodb://localhost/Contacts'
+        schema = new mongoose.Schema({
+            _id: Number,
+            nom: String,
+            prenom: String,
+            tel: String,
+            mail: String,
+            birthdate: {type: Date, default: Date.now}
+        });
+        dbContact = mongoose.model('dbContact', schema);
+    };
 
     exports.insertContact = function (contact, callback){
 
     };
+
+    exports.getContacts = function (callback) {
+        db.Contacts.find(callback);
+    }
 })();
--- node/dao/database.js
+++ node/dao/database.js
@@ -1,3 +1,4 @@
 /**
  * Created by Shadow on 02/03/2015.
  */
+// Don't know if this is useful
